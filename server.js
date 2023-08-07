cf284bef9f4c0499631d0bb45fdf3adf8e9c2cf9
vallinayakichidambaram <acgvalli@gmail.com>
2023-08-07 14:59:17 +0530

    starter

--- server.js
+++ server.js
@@ -4,6 +4,18 @@ const app = express()
 
 const PORT = 3000
 
+
+app.get('/', (req,res) => {
+  res.send('Helloo Welcome!!')
+})
+
+app.get('/cats', (req,res) => {
+  res.send({
+    id: 101,
+    name: "Valli"
+  })
+})
+
 app.listen(PORT, ()=> {
     console.log(`Listening on PORT ${PORT}`)
 })
